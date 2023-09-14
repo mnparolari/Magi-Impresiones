78a5fd3e83cb5de3e43edaa6a85bc52b2fd1da80
Martin Parolari <mnparolari@gmail.com>
2023-09-14 20:25:38 -0300

    project structure

---
+++ src/routes/RoutesNavigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from '../components/Header/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
+import Home from '../components/Home/Home'
+import About from '../components/About/About'
+import Contacts from '../components/Contacts/Contacts'
+import ItemListContainer from '../components/Products/ItemListContainer'
+
+const RoutesNavigation = () => {
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/SobreMagi' element={<About />} />
+                <Route path="/:category" element={<ItemListContainer />} />
+                <Route path='/Contactos' element={<Contacts />} />
+                <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
+}
+
+export default RoutesNavigation
