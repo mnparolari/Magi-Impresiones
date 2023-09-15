5c3ba0bab0345dcc01a51ff4c768b735f9b99335
Martin Parolari <mnparolari@gmail.com>
2023-09-15 11:49:27 -0300

    completed footer + responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -14,33 +14,37 @@ const Footer = () => {
         <div className='iconsBox'>
           <div>
             <div className='iconsContainer'>
-              <a href="https://goo.gl/maps/CDRBpGH5nqJxNxfJ7" target='_blank'><img src={location} alt="Ubicación" /></a>
+              <a href="https://goo.gl/maps/CDRBpGH5nqJxNxfJ7" target='_blank' rel="noreferrer"><img src={location} alt="Ubicación" /></a>
               <p className='iconsText'>Maipú - Mendoza</p>
             </div>
             <div className='iconsContainer'>
-              <a href="https://wa.me/5492612421929" target='_blank'><img src={whatsapp} alt="Whatsapp" /></a>
+              <a href="https://wa.me/5492612421929" target='_blank' rel="noreferrer"><img src={whatsapp} alt="Whatsapp" /></a>
               <p className='iconsText'>0261 242-1929</p>
             </div>
             <div className='iconsContainer'>
-              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=villarroyam.imei@gmail.com" target='_blank'><img src={email} alt="Email" /></a>
+              <a href="https://mail.google.com/mail/?view=cm&fs=1&to=villarroyam.imei@gmail.com" target='_blank' rel="noreferrer"><img src={email} alt="Email" /></a>
               <p className='iconsText'>villarroyam.imei@gmail.com</p>
             </div>
-            .
           </div>
           <div>
             <div className='iconsContainer'>
-              <a href="https://www.instagram.com/magiimpresiones/?hl=es" target='_blank'><img src={ig} alt="Instagram" /></a>
+              <a href="https://www.instagram.com/magiimpresiones/?hl=es" target='_blank' rel="noreferrer"><img src={ig} alt="Instagram" /></a>
               <p className='iconsText'>magiimpresiones</p>
             </div>
             <div className='iconsContainer'>
-              <a href="https://m.facebook.com/magiimpresiones" target='_blank'><img src={facebook} alt="Facebook" /></a>
+              <a href="https://m.facebook.com/magiimpresiones" target='_blank' rel="noreferrer"><img src={facebook} alt="Facebook" /></a>
               <p className='iconsText'>Magi Impresiones</p>
             </div>
             <div className='iconsContainer'>
-              <a href="https://heyzine.com/flip-book/f15f0e13a3.html" target='_blank'><img src={catalogue} alt="Catálogo" /></a>
+              <a href="https://heyzine.com/flip-book/f15f0e13a3.html" target='_blank' rel="noreferrer"><img src={catalogue} alt="Catálogo" /></a>
               <p className='iconsText'>Nuestro Catálogo</p>
             </div>
           </div>
+        </div>
+        <div className='iconsBoxMobile'>
+          <a href="https://wa.me/5492612421929" target='_blank' rel="noreferrer"><img src={whatsapp} alt="Whatsapp" /></a>
+          <a href="https://mail.google.com/mail/?view=cm&fs=1&to=villarroyam.imei@gmail.com" target='_blank' rel="noreferrer"><img src={email} alt="Email" /></a>
+          <a href="https://www.instagram.com/magiimpresiones/?hl=es" target='_blank' rel="noreferrer"><img src={ig} alt="Instagram" /></a>
         </div>
       </div>
       <div className='smallContainer'>
